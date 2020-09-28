6c4c15861c69dd70a861122bf17946ba162258fb
suulcoder <ef.proyectomundial@gmail.com>
2020-09-28 00:43:20 -0600

    Updates

---
+++ precache-manifest.10fbf43854254a517e0a1a8333e25e1a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37126b35265950da1845545df9b01bd3",
+    "url": "/adventure/index.html"
+  },
+  {
+    "revision": "79382dcd8e63f0ac0a7e",
+    "url": "/adventure/static/css/main.3708be19.chunk.css"
+  },
+  {
+    "revision": "4e1827abde7f2e45c15c",
+    "url": "/adventure/static/js/2.4533fa19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/adventure/static/js/2.4533fa19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79382dcd8e63f0ac0a7e",
+    "url": "/adventure/static/js/main.718140ab.chunk.js"
+  },
+  {
+    "revision": "e4166a28b80c1773ea4a",
+    "url": "/adventure/static/js/runtime-main.afd66130.js"
+  },
+  {
+    "revision": "411ec2c37c72db64ffcdd497712ac69d",
+    "url": "/adventure/static/media/SquareBlack.411ec2c3.png"
+  },
+  {
+    "revision": "54231c0670e8902d0d828b27f9f54904",
+    "url": "/adventure/static/media/earth.54231c06.png"
+  },
+  {
+    "revision": "674de013331eb32fcf35b3b1b192da42",
+    "url": "/adventure/static/media/logo.674de013.png"
+  },
+  {
+    "revision": "5dae729ea7df123e75f5235451a2b445",
+    "url": "/adventure/static/media/satellite.5dae729e.png"
+  }
+]);
