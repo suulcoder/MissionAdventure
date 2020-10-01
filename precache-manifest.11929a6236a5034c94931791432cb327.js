b5071a4e401183b33a44627246b65003a167e4dd
suulcoder <ef.proyectomundial@gmail.com>
2020-09-30 18:23:38 -0600

    Updates

---
+++ precache-manifest.11929a6236a5034c94931791432cb327.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd3230b6e6251e411d018792857201a",
+    "url": "/adventure/index.html"
+  },
+  {
+    "revision": "a11f9b7e68336abbb786",
+    "url": "/adventure/static/css/main.1249a4cd.chunk.css"
+  },
+  {
+    "revision": "6099ff510481cee730aa",
+    "url": "/adventure/static/js/2.da029461.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/adventure/static/js/2.da029461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a11f9b7e68336abbb786",
+    "url": "/adventure/static/js/main.591701d0.chunk.js"
+  },
+  {
+    "revision": "e4166a28b80c1773ea4a",
+    "url": "/adventure/static/js/runtime-main.afd66130.js"
+  },
+  {
+    "revision": "411ec2c37c72db64ffcdd497712ac69d",
+    "url": "/adventure/static/media/SquareBlack.411ec2c3.png"
+  },
+  {
+    "revision": "665d4d9cf944ad1a58c6da0d7fae4c2d",
+    "url": "/adventure/static/media/australia.665d4d9c.png"
+  },
+  {
+    "revision": "5cb411c3c8c4b3d20beb3cdecaf25174",
+    "url": "/adventure/static/media/california.5cb411c3.png"
+  },
+  {
+    "revision": "75ca48cf1b3673711347452fdc5445ed",
+    "url": "/adventure/static/media/code.75ca48cf.jpg"
+  },
+  {
+    "revision": "e1f61e21fafa8579524189784e9abac5",
+    "url": "/adventure/static/media/communication.e1f61e21.png"
+  },
+  {
+    "revision": "f2281ec4611163fb5be55df91e5b2625",
+    "url": "/adventure/static/media/dsn.f2281ec4.png"
+  },
+  {
+    "revision": "54231c0670e8902d0d828b27f9f54904",
+    "url": "/adventure/static/media/earth.54231c06.png"
+  },
+  {
+    "revision": "6f95d7c25812b1738bbc78065223315a",
+    "url": "/adventure/static/media/entang.6f95d7c2.png"
+  },
+  {
+    "revision": "ccf029864d48956464a0b1b08c5a4c8c",
+    "url": "/adventure/static/media/installation.ccf02986.jpg"
+  },
+  {
+    "revision": "d31031568818d69aac0738eba3133dc7",
+    "url": "/adventure/static/media/inter.d3103156.gif"
+  },
+  {
+    "revision": "593b57762c8dc04a4fca9b8f4b2c3272",
+    "url": "/adventure/static/media/light.593b5776.webp"
+  },
+  {
+    "revision": "a75632e887e76062825f1151feac09a0",
+    "url": "/adventure/static/media/light.a75632e8.gif"
+  },
+  {
+    "revision": "674de013331eb32fcf35b3b1b192da42",
+    "url": "/adventure/static/media/logo.674de013.png"
+  },
+  {
+    "revision": "ca5111d2c6a1bde5183e1b65304e0954",
+    "url": "/adventure/static/media/rocket.ca5111d2.png"
+  },
+  {
+    "revision": "70d49678fe49351c56dca8c4ac5b4b85",
+    "url": "/adventure/static/media/rotation.70d49678.gif"
+  },
+  {
+    "revision": "5dae729ea7df123e75f5235451a2b445",
+    "url": "/adventure/static/media/satellite.5dae729e.png"
+  },
+  {
+    "revision": "57124beb118f70041dc6201bd5360faa",
+    "url": "/adventure/static/media/spain.57124beb.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1de7753f5464365340da107fbc18cce0.js"
+  "/adventure/precache-manifest.11929a6236a5034c94931791432cb327.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/adventure/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
